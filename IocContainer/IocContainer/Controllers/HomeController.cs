cd27df4ef6ddec9b78f5d2fafe59789023fab667
Oğuzhan Bilal Kahraman <73184456+OguzhanbilalKahraman@users.noreply.github.com>
2022-08-22 16:02:57 +0300

    Add files via upload

---
+++ IocContainer/IocContainer/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using IocContainer.Services;
+using IocContainer.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IocContainer.Controllers
+{
+    public class HomeController : Controller
+    {
+        //ConsoleLogger _log;
+        Ilog _log;
+        public HomeController(Ilog log)
+        {
+            _log = log;
+        }
+        public string  Index([FromServices] Ilog log)
+        {
+            //ConsoleLogger consolelogger = new ConsoleLogger();
+            //consolelogger.Info("Yeni bir istek gelmistir.");
+            //return View();
+            log.Info("Action bazlı talep yapılmıştır.");
+            return "Service Running";
+
+
+        }
+    }
+}
---
+++ IocContainer/IocContainer/Services/ConsoleLogger.cs
@@ -0,0 +1,17 @@
+﻿using IocContainer.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IocContainer.Services
+{
+    public class ConsoleLogger : Ilog
+    {
+
+        public void Info(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
